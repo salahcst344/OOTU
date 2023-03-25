be1fad48fb24842dd8e10b06e5fdd3855f2a44cc
salahcst344 <salahcst344@gmail.com>
2023-03-25 12:57:56 +0200

    Configer i18n for En and Ar

---
+++ src/translation/i18n.js
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enData from './languages/En.json'
+import arData from './languages/Ar.json'
+
+const resources = {
+  en: {
+    translation: enData,
+  },
+  ar: {
+    translation: arData,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
