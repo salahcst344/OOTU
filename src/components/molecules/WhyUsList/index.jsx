45524f72ff4dbf3a2e5a26c19e7ff702a623088e
salahcst344 <salahcst344@gmail.com>
2023-03-30 01:36:13 +0200

    Build why us list

---
+++ src/components/molecules/WhyUsList/index.jsx
@@ -0,0 +1,19 @@
+import WhyUsItem from "../../atoms/WhyUsItem";
+
+const WhyUsList = ({ whyUs }) => {
+  return (
+    <ul
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "4.8rem",
+      }}
+    >
+      {whyUs.map((item) => (
+        <WhyUsItem whyUs={item} />
+      ))}
+    </ul>
+  );
+};
+
+export default WhyUsList;
