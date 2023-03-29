8e3965268cf38ec4f39e283afdb4d251b8b7b4c4
salahcst344 <salahcst344@gmail.com>
2023-03-29 16:11:15 +0200

    Build FAButton stacl

---
+++ src/components/molecules/FAButtonStack/index.jsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import FAButton from "../../atoms/FAButton";
+import FAButtonItem from "../../atoms/FAButtonItem";
+import classes from "./FAButtonStack.module.css";
+
+const FAButtonStack = ({ contactInfo }) => {
+  const [open, setOpen] = useState(false);
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "100%" },
+  };
+
+  return (
+    <div className={classes["fab-stack"]}>
+      <FAButton open={open} handleClick={() => setOpen((prev) => !prev)} />
+      <motion.div animate={open ? "open" : "closed"} variants={variants}>
+        {contactInfo.map((item) => (
+          <FAButtonItem icon={item.icon} path={item.path} />
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default FAButtonStack;
