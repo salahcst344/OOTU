0d5e6776b0999d0bf8955aa3af87dcc9a7e0c4e7
salahcst344 <salahcst344@gmail.com>
2023-03-29 11:02:14 +0200

    Add CTA button

--- src/components/molecules/NavLinks/index.jsx
+++ src/components/molecules/NavLinks/index.jsx
@@ -1,12 +1,20 @@
+import MainBtn from "../../atoms/MainBtn";
 import NavLink from "../../atoms/NavLink";
 import classes from "./NavLinks.module.css";
 
-const NavLinks = ({ navLinks }) => {
+const NavLinks = ({ navLinks, CTABtn }) => {
+  console.log(CTABtn);
   return (
     <ul className={classes["nav-links"]}>
       {navLinks.map((link) => (
         <NavLink link={link} />
       ))}
+      <NavLink
+        link={{
+          path: CTABtn.path,
+          text: <MainBtn>{CTABtn.text}</MainBtn>,
+        }}
+      />
     </ul>
   );
 };
