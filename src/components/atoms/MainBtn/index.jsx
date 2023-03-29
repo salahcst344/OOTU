d573ae2dc4c9da239057d4b7abee7a808384c116
salahcst344 <salahcst344@gmail.com>
2023-03-29 16:12:30 +0200

    Animate button bg color

--- src/components/atoms/MainBtn/index.jsx
+++ src/components/atoms/MainBtn/index.jsx
@@ -1,7 +1,22 @@
 import classes from "./MainBtn.module.css";
+import { motion } from "framer-motion";
 
 const MainBtn = ({ children }) => {
-  return <button className={classes["main-btn"]}>{children}</button>;
+  return (
+    <motion.button
+      className={classes["main-btn"]}
+      initial={{
+        backgroundSize: "200% 200%",
+        color: "var(--white)",
+      }}
+      whileHover={{ backgroundPosition: "100% 100%", color: "var(--black)" }}
+      transition={{
+        stiffness: "100",
+      }}
+    >
+      {children}
+    </motion.button>
+  );
 };
 
 export default MainBtn;
