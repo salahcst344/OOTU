73a5536e3c6233239371afbfed76482f4fd7de85
salahcst344 <salahcst344@gmail.com>
2023-03-30 01:37:32 +0200

    Build why us organism

--- src/components/organisms/WhyUs/index.jsx
+++ src/components/organisms/WhyUs/index.jsx
@@ -1,20 +1,25 @@
-import CenteredContainer from "../../atoms/CenteredContainer";
 import LightDescription from "../../atoms/LightDescription";
 import SecondaryHeading from "../../atoms/SecondaryHeading";
 import SubHeading from "../../atoms/SubHeading";
+import WhyUsList from "../../molecules/WhyUsList";
+import { ReactComponent as WhyUsImg } from "../../../assets/why-us.svg";
 import classes from "./WhyUs.module.css";
 
-const WhyUs = () => {
+const WhyUs = ({ whyUs }) => {
   return (
     <section className={classes["why-us"]}>
-      <CenteredContainer>
-        <SubHeading>we are the best</SubHeading>
-        <SecondaryHeading>Why Choose Us</SecondaryHeading>
-        <LightDescription>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
-          ad maxime repellat at et ipsam.
-        </LightDescription>
-      </CenteredContainer>
+      <SubHeading>we are the best</SubHeading>
+      <SecondaryHeading>Why Choose Us</SecondaryHeading>
+      <LightDescription>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, ad
+        maxime repellat at et ipsam.
+      </LightDescription>
+      <div>
+        <WhyUsList whyUs={whyUs} />
+        <div>
+          <WhyUsImg />
+        </div>
+      </div>
     </section>
   );
 };
