ae99f0070f87d6f9fb09153da873a3426775cb5d
salahcst344 <salahcst344@gmail.com>
2023-03-29 17:26:49 +0200

    Add Why Us secion

--- src/components/templates/Home.jsx
+++ src/components/templates/Home.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../organisms/HeroSection";
+import WhyUs from "../organisms/WhyUs";
 
 const Home = ({ heroContent, experiences }) => {
   return (
     <>
       <HeroSection heroContent={heroContent} />
+      <WhyUs />
     </>
   );
 };
