5a2c00be29267ebaa52de8b8405bb958bf0fce5e
salahcst344 <salahcst344@gmail.com>
2023-03-29 11:13:21 +0200

    Build home templete

---
+++ src/components/templates/Home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "../organisms/HeroSection";
+
+const Home = ({ heroContent, experiences }) => {
+  return (
+    <>
+      <HeroSection heroContent={heroContent} />
+    </>
+  );
+};
+
+export default Home;
