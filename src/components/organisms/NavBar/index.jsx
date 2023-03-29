a312e51c636fd8a09df46f26fdfc88c5334df7ba
salahcst344 <salahcst344@gmail.com>
2023-03-30 01:37:01 +0200

    Make navbar sticky

--- src/components/organisms/NavBar/index.jsx
+++ src/components/organisms/NavBar/index.jsx
@@ -1,10 +1,20 @@
 import NavLinks from "../../molecules/NavLinks";
 import Logo from "../../atoms/Logo";
 import classes from "./NavBar.module.css";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
 
 const NavBar = ({ navLinks, CTABtn }) => {
+  const { scrollY } = useScroll();
+  const [YLength, setYLength] = useState(scrollY);
+  useMotionValueEvent(scrollY, "change", (newValue) => setYLength(newValue));
+
   return (
-    <nav className={classes["main-nav"]}>
+    <nav
+      className={`${classes["main-nav"]} ${
+        YLength > 200 ? classes.sticky : ""
+      }`}
+    >
       <Logo />
       <NavLinks navLinks={navLinks} CTABtn={CTABtn} />
     </nav>
