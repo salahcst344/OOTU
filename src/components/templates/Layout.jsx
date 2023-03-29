8ad1c4f0f6daf762f61923cad56676e7dc265aed
salahcst344 <salahcst344@gmail.com>
2023-03-29 11:11:16 +0200

    Get rid of the background

---
+++ src/components/templates/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../organisms/NavBar";
+
+const Layout = ({ children, navLinks, CTABtn }) => {
+  return (
+    <>
+      <NavBar navLinks={navLinks} CTABtn={CTABtn} />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
