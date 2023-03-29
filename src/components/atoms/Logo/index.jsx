0499108fe72bb5cff0a3bd52ae6fdaefd6dde151
salahcst344 <salahcst344@gmail.com>
2023-03-29 10:56:40 +0200

    Some edit to the logo

--- src/components/atoms/Logo/index.jsx
+++ src/components/atoms/Logo/index.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom";
 import logo from "../../../assets/ootu-logo1.png";
 import classes from "./logo.module.css";
 
-const index = () => {
-  return <Link to='/' className={classes["logo"]}> <img src={logo} alt="logo" /></Link>;
+const Logo = () => {
+  return (
+    <Link to="/" className={classes["logo"]}>
+      <img src={logo} alt="logo" />
+    </Link>
+  );
 };
 
-export default index;
+export default Logo;
