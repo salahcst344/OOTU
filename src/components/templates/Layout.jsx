bccb9ca3991a64e4cfe41e95c11aa22e5cad073d
salahcst344 <salahcst344@gmail.com>
2023-03-29 16:11:45 +0200

    Add FAButton stack

--- src/components/templates/Layout.jsx
+++ src/components/templates/Layout.jsx
@@ -1,9 +1,11 @@
+import FAButtonStack from "../molecules/FAButtonStack";
 import NavBar from "../organisms/NavBar";
 
-const Layout = ({ children, navLinks, CTABtn }) => {
+const Layout = ({ children, navLinks, CTABtn, contactInfo }) => {
   return (
     <>
       <NavBar navLinks={navLinks} CTABtn={CTABtn} />
+      <FAButtonStack contactInfo={contactInfo} />
       {children}
     </>
   );
