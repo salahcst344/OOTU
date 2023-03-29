583d32196797fb854c5bc2bb465cfc501ee88093
salahcst344 <salahcst344@gmail.com>
2023-03-29 11:04:10 +0200

    Pass CTA button data

--- src/components/organisms/NavBar/index.jsx
+++ src/components/organisms/NavBar/index.jsx
@@ -2,11 +2,11 @@ import NavLinks from "../../molecules/NavLinks";
 import Logo from "../../atoms/Logo";
 import classes from "./NavBar.module.css";
 
-const NavBar = ({ navLinks }) => {
+const NavBar = ({ navLinks, CTABtn }) => {
   return (
     <nav className={classes["main-nav"]}>
       <Logo />
-      <NavLinks navLinks={navLinks} />
+      <NavLinks navLinks={navLinks} CTABtn={CTABtn} />
     </nav>
   );
 };
