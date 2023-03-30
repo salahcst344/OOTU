6b115ce45ce62e504022a1a926af72b76f1b55dd
salahcst344 <salahcst344@gmail.com>
2023-03-30 13:05:43 +0200

    Make it lighter

--- src/components/atoms/LightDescription/index.jsx
+++ src/components/atoms/LightDescription/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const LightDescription = ({ children, gutterButtom = true }) => {
+const LightDescription = ({ children, styles, gutterButtom = true }) => {
   return (
     <p
       style={{
         fontSize: "2rem",
         lineHeight: 1.5,
         marginBottom: gutterButtom ? "4.8rem" : undefined,
-        opacity: 0.9,
+        opacity: 0.7,
+        ...styles,
       }}
     >
       {children}
