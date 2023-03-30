d85dff807f884fd799b3e5965b590104b5b78753
salahcst344 <salahcst344@gmail.com>
2023-03-30 13:08:51 +0200

    Fix structure

--- src/components/organisms/WhyUs/index.jsx
+++ src/components/organisms/WhyUs/index.jsx
@@ -4,21 +4,32 @@ import SubHeading from "../../atoms/SubHeading";
 import WhyUsList from "../../molecules/WhyUsList";
 import whyUsUmg from "../../../assets/why-us2.jpg";
 import classes from "./WhyUs.module.css";
+import CenteredContainer from "../../atoms/CenteredContainer";
 
 const WhyUs = ({ whyUs }) => {
   return (
     <section className={classes["why-us"]}>
-      <SubHeading>we are the best</SubHeading>
-      <SecondaryHeading>Why Choose Us</SecondaryHeading>
-      <LightDescription>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, ad
-        maxime repellat at et ipsam.
-      </LightDescription>
       <div>
-        <WhyUsList whyUs={whyUs} />
-        <div>
-          <img src={whyUsUmg} alt="why us" />
+        <div className={classes.heading}>
+          <SubHeading>we are the best</SubHeading>
+          <SecondaryHeading
+            styles={{
+              color: "var(--primary-shade)",
+            }}
+          >
+            Why Choose Us
+          </SecondaryHeading>
+          <LightDescription styles={{}}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
+            ad maxime repellat at et ipsam.
+          </LightDescription>
         </div>
+        <CenteredContainer>
+          <WhyUsList whyUs={whyUs} />
+        </CenteredContainer>
+      </div>
+      <div className={classes['why-img']}>
+        <img src={whyUsUmg} alt="why us" />
       </div>
     </section>
   );
