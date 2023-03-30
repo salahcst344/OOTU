93111c36ee3d051540b3d0d9b570d2647d26faf1
salahcst344 <salahcst344@gmail.com>
2023-03-30 13:31:48 +0200

    fix styles

--- src/components/atoms/WhyUsItem/index.jsx
+++ src/components/atoms/WhyUsItem/index.jsx
@@ -6,12 +6,14 @@ const WhyUsItem = ({ whyUs }) => {
   return (
     <li className={classes["why-us-item"]}>
       <div className={classes["why-us-dot"]}>
-        <img src={whyUs.img} alt={whyUs.title} />
+        <div>
+          <img src={whyUs.img} alt={whyUs.title} />
+        </div>
       </div>
       <div>
         <TertiaryHeading
           styles={{
-            color: "var(--primary-shade)",
+            color: "var(--primary-shader)",
           }}
         >
           {whyUs.title}
